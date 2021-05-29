b005c95f8e6172514030ce474703212e68e45aa7
Bryan Henry <bryanhenry522@gmail.com>
2021-05-28 23:13:57 -0700

    Add tracing crate for tracing & logging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 use serenity::{async_trait, model::gateway::Ready, prelude::*};
+use tracing::{debug, info};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-        println!("Ready data: {:#?}", ready);
+        info!("{} is connected!", ready.user.name);
+        debug!("Ready data: {:#?}", ready);
     }
 }
 
 #[tokio::main]
 async fn main() {
+    // Load .env if one exists, but not required. (Environment vars could be passed directly)
     dotenv::dotenv().ok();
-    let token = std::env::var("DISCORD_BOT_TOKEN").expect("Missing $DISCORD_BOT_TOKEN");
 
+    // Setup tracing/logging.
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_target(true)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to start the logger");
+
+    let token = std::env::var("DISCORD_BOT_TOKEN").expect("Missing $DISCORD_BOT_TOKEN");
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
