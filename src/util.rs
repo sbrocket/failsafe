1606f25f0367c3c922c3d3cd2d8eae2e248ddb00
Bryan Henry <bryanhenry522@gmail.com>
2021-06-04 14:02:27 -0700

    Add tempfile utility for tokio's async Files

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,12 @@
 use anyhow::{format_err, Result};
+use rand::{distributions::Alphanumeric, prelude::*};
 use serde_json::Value;
 use serenity::model::{
     interactions::{ApplicationCommandInteractionDataOption, Interaction},
     prelude::*,
 };
+use std::{io::ErrorKind, path::PathBuf};
+use tokio::fs::File;
 
 pub trait InteractionExt {
     fn get_user(&self) -> Result<&User>;
@@ -35,4 +38,29 @@ impl OptionsExt for &Vec<ApplicationCommandInteractionDataOption> {
             .as_ref()
             .ok_or_else(|| format_err!("No value for option '{}'", name))
     }
+}
+
+pub async fn tempfile() -> Result<(PathBuf, File)> {
+    const TEMP_PREFIX: &str = "tmpfile_";
+    const RAND_LEN: usize = 10;
+    const RETRIES: usize = 4;
+
+    for _ in 0..RETRIES {
+        let mut tempname = String::with_capacity(TEMP_PREFIX.len() + RAND_LEN);
+        tempname.push_str(TEMP_PREFIX);
+        tempname.extend(
+            thread_rng()
+                .sample_iter(Alphanumeric)
+                .take(RAND_LEN)
+                .map(char::from),
+        );
+
+        let mut path = std::env::temp_dir();
+        path.push(tempname);
+        match File::create(&path).await {
+            Err(e) if e.kind() == ErrorKind::AlreadyExists => continue,
+            file => return Ok((path, file?)),
+        };
+    }
+    Err(format_err!("Failed to create tempfile"))
 }
